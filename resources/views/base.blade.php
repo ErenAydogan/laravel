f602c1778da3379d8129657a4c8d347bbab2d49b
ErenAydogan <erenaydogan@live.fr>
2024-08-12 21:27:10 +0300

    Updated

---
+++ resources/views/base.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>
+        Test Page
+    </title>
+</head>
+<body>
+    @include('Components.nav-link')
+    @section('content')
+        
+    @endsection
+</body>
+</html>
