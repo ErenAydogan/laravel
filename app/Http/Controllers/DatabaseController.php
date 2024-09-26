ff99c980b60e36ed610af67b2d297a09baee47cd
ErenAydogan <erenaydogan@live.fr>
2024-09-27 00:06:59 +0300

    Updated

--- app/Http/Controllers/DatabaseController.php
+++ app/Http/Controllers/DatabaseController.php
@@ -9,16 +9,34 @@ use App\Models\User;
 class DatabaseController extends Controller
 {
     public function index() {
+        /* Getting all data */
+
 /*         $users=DB::select('select name, email from users');
         dd($users); */
 
 /*         $users = DB::table('users')->select('name', 'email')->WhereNotNull('email')->orderBy('name')->get();
         dd($users); */
 
+        
         $users = User::all();
         foreach($users as $user)
         {
             echo $user->id . ". " .  $user->name . ' - ' . $user->email . '<br>';
         }
+        
+    }
+
+    public function create(){
+        
+        $user = new User();
+        $user -> name = "Alice"; 
+        $user -> email = "alice@test.com"; 
+        $user -> save(); 
+    }
+
+    public function update(){
+        $user = User::where('email', "john@example.com")->first();
+        $user -> email = 'john2@example.com';
+        $user -> save();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,20 @@
 <?php
 
-use App\Http\Controllers\DatabaseController;
-use App\Models\Student;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function(){
-    $students = Student::select('name', 'email')->whereNotNull('email')->orderBy('name')->get();
-    dd($students);
+Route::get('/', function () {
+    return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('DatabaseTest', [DatabaseController::class, 'index']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
