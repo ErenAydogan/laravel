a5f4e51acc091c1a664806fa273553874071bbe5
ErenAydogan <erenaydogan@live.fr>
2024-10-02 00:00:49 +0300

    Updated

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\NotebookController;
+use App\Http\Controllers\NoteController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,3 +20,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+Route::resource('notes', NoteController::class)->middleware('auth');
+Route::resource('notebooks', NotebookController::class)->middleware('auth');
