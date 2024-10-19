4c35a3bee0d92707fabf94c56ee4c7b526b22fe4
ErenAydogan <erenaydogan@live.fr>
2024-10-19 18:40:33 +0300

    Files updated

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,11 @@
 <?php
 
-use App\Http\Controllers\NotebookController;
-use App\Http\Controllers\NoteController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "<h1>Welcome</h1>";
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::get('/home', function () {
+    return view('home', ['name' => 'Shruti']);
 });
-
-require __DIR__.'/auth.php';
-
-
-Route::resource('notes', NoteController::class)->middleware('auth');
-Route::resource('notebooks', NotebookController::class)->middleware('auth');
