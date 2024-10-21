5f5c9ebf1c37b8eccf2427a017a75f8bd399e180
ErenAydogan <erenaydogan@live.fr>
2024-10-22 01:13:39 +0300

    Working on Layouts

---
+++ resources/views/about.blade.php
@@ -0,0 +1,3 @@
+<x-base>
+    <h1 class="text-4xl font-bold">About Us</h1>
+</x-base>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <title>Document</title>
-</head>
-<body>
-    <nav class="bg-gray-100 px-8 py-4 text-gray-700 flex items-center justify-between">
-        <span class="font-bold text-2xl">Brand</span>
-        <span>Hello, User</span>
-    </nav>
-    <section class="p-12 mx-auto max-w-6xl text-gray-800">
-        <h1 class="text-4xl font-bold">Home</h1>
-        <p class="mt-4 text-lg">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae alias, vero neque maiores nulla quas pariatur ab aliquid ducimus debitis. Officiis sequi deleniti, voluptas sunt quo provident culpa veniam assumenda!
-        Inventore, deleniti temporibus possimus, placeat reiciendis voluptas dolorem obcaecati modi minus autem tempora neque iste! Dolorem optio nam temporibus qui placeat quasi, ullam tenetur quaerat eveniet architecto aspernatur libero delectus.
-        Accusamus animi eveniet explicabo tempora ducimus, saepe doloribus quisquam necessitatibus assumenda incidunt quas corrupti illo eius ea quod tenetur reprehenderit voluptatem quasi veritatis, beatae nesciunt iure quos officia. Soluta, iusto.</p>
-    </section>
-</body>
-</html>
+<x-base>
+<x-slot name="name">{{ $name }}</x-slot>
+
+<x-slot name="content">
+<h1 class="text-4xl font-bold">Home</h1>
+<p class="mt-4 text-lg">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae alias, vero neque maiores nulla quas pariatur ab aliquid ducimus debitis. Officiis sequi deleniti, voluptas sunt quo provident culpa veniam assumenda!
+Inventore, deleniti temporibus possimus, placeat reiciendis voluptas dolorem obcaecati modi minus autem tempora neque iste! Dolorem optio nam temporibus qui placeat quasi, ullam tenetur quaerat eveniet architecto aspernatur libero delectus.
+Accusamus animi eveniet explicabo tempora ducimus, saepe doloribus quisquam necessitatibus assumenda incidunt quas corrupti illo eius ea quod tenetur reprehenderit voluptatem quasi veritatis, beatae nesciunt iure quos officia. Soluta, iusto.</p>
+</x-slot>
+
+
+</x-base>
