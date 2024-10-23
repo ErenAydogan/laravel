5690346b7121d97130df6df92d0e1ef4f6cd9f0f
ErenAydogan <erenaydogan@live.fr>
2024-10-23 09:50:37 +0300

    Added Controllers

---
+++ resources/views/greeting.blade.php
@@ -0,0 +1,7 @@
+<x-base>
+    <x-slot name="name">
+    </x-slot>
+    <x-slot name="content">
+        <h1 class="font-bold">Hello World</h1>
+    </x-slot>
+</x-base>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Http\Controllers\GreetingController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return "<h1>Welcome</h1>";
-});
+Route::get('/',[WelcomeController::class, 'index']);
 
 Route::get('/home', function () {
     return view('home', ['name' => 'Shruti']);
 });
+
+Route::get('/hello', [GreetingController::class, 'index']);
