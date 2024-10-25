6f2fd759b5079bc991f61976a218e3cf8fae7bf1
ErenAydogan <erenaydogan@live.fr>
2024-10-25 23:30:00 +0300

    Working on Database

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,18 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\GreetingController;
-use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',[WelcomeController::class, 'index']);
+Route::get('/', function () {
+    /* Using row SQL queries */
+    /* $users = DB::select('select * from users');
+    dd($users); */
+
+    /* Query builder */
+    $users = DB::table('users')->whereNotNull('email')->orderBy('name')->get();
+    dd($users);
+});
 
 Route::get('/home', function () {
     return view('home', ['name' => 'Shruti']);
