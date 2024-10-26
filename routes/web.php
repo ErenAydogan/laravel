cd574b783f71b377a0407036e39aa3da1a337aa3
ErenAydogan <erenaydogan@live.fr>
2024-10-26 12:43:57 +0300

    Models has been updated

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\GreetingController;
+use App\Models\Student;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,8 +11,24 @@ Route::get('/', function () {
     dd($users); */
 
     /* Query builder */
-    $users = DB::table('users')->whereNotNull('email')->orderBy('name')->get();
-    dd($users);
+    /* $users = DB::table('users')->whereNotNull('email')->orderBy('name')->get();
+    dd($users); */
+
+    /* Eloquent ORM */
+    /* $students = Student::all(); */
+    /* dd($students); */
+    
+    $students = Student::select(['name', 'email'])->whereNotNull('email')->orderBy('name')->get();
+
+    foreach ($students as $student)
+    {
+        echo $student->name . "<br>";
+    } 
+
+    /* $student = new Student();
+    $student->name = "Jane";
+    $student->email = "jane@example.com";
+    $student->save(); */
 });
 
 Route::get('/home', function () {
