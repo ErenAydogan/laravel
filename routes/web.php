186158a14a16e3222fb013444742050a37e3b7ff
ErenAydogan <erenaydogan@live.fr>
2024-10-30 23:48:52 +0300

    Updated

--- routes/web.php
+++ routes/web.php
@@ -1,52 +1,24 @@
 <?php
 
-use Illuminate\Support\Facades\DB;
-use App\Http\Controllers\GreetingController;
-use App\Models\Student;
+use App\Http\Controllers\NoteController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    /* Using row SQL queries */
-    /* $users = DB::select('select * from users');
-    dd($users); */
-
-    /* Query builder */
-    /* $users = DB::table('users')->whereNotNull('email')->orderBy('name')->get();
-    dd($users); */
-
-    /* Eloquent ORM */
-    /* $students = Student::all(); */
-    /* dd($students); */
-    
-    $students = Student::select(['name', 'email'])->whereNotNull('email')->orderBy('name')->get();
-
-    foreach ($students as $student)
-    {
-        echo $student->name . "<br>";
-    } 
-
-    /* $student = new Student();
-    $student->name = "Jane";
-    $student->email = "jane@example.com";
-    $student->save(); */
+    return view('welcome');
 });
 
-Route::get('/test', function () {
-    // Get the student model where email is john@example.com
-    $student = Student::where('email', 'john@example.com')->first();
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-    // Update the "status" column with the value 'active'
-    $student->status = 'active';
-
-    // Save the model
-    $student->save();
-
-    // Dump the result
-    dd($student);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/home', function () {
-    return view('home', ['name' => 'Shruti']);
-});
+require __DIR__.'/auth.php';
 
-Route::get('/hello', [GreetingController::class, 'index']);
+
+Route::resource('notes', NoteController::class)->middleware('auth');
