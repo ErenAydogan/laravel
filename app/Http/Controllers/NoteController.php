ac3780734ca00005ab309417d89a1ed07f09b8b7
ErenAydogan <erenaydogan@live.fr>
2024-11-04 23:53:31 +0300

    Storage for notes

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -23,7 +23,7 @@ class NoteController extends Controller
      */
     public function create()
     {
-        //
+        return view('notes.create');
     }
 
     /**
@@ -31,7 +31,17 @@ class NoteController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'title' => 'required|max:120',
+            'text' => 'required',
+        ]);
+
+        $note = new Note([
+            'user_id' => Auth::id(),
+            'title' => $request->title,
+            'text' => $request->text
+        ]);
+        $note->save();
     }
 
     /**
